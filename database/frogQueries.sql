2ca68e4dd798b2283dbdf35ceff048e18b579fdc
kyra-h <helmboldk@carleton.edu>
2023-04-27 13:52:04 -0500

    edited frog queries

--- database/frogQueries.sql
+++ database/frogQueries.sql
@@ -1,8 +1,10 @@
 /* List all of the frogs with the scientific name Litoria rheocola */
-
 SELECT * FROM frogData WHERE scientificName = 'Litoria rheocola' ORDER BY occurrenceID DESC;
 
-SELECT * FROM frogData WHERE decimalLatitude < -30 AND decimalLatitude > -40 ORDER BY decimalLatitude DESC;
 
-SELECT * FROM frogData WHERE eventDate LIKE '2017' ORDER BY eventDate DESC;
+/* List all of the frogs within latitidues -30 & -40 */
+SELECT * FROM frogData WHERE decimalLatitude <= -30 AND decimalLatitude >= -40 ORDER BY decimalLatitude DESC;
+
+/* List all of the frog sightings on 11/30/2017 */
+SELECT * FROM frogData WHERE eventDate LIKE '11/30/2017' ORDER BY eventDate DESC;
 
